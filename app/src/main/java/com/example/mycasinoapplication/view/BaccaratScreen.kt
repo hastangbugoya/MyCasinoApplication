552354413b8388ae280867d133bb42d0e224d6f1
Jeff <47189403+hastangbugoya@users.noreply.github.com>
2024-02-13 23:58:30 -0800

    UI working-ish

--- app/src/main/java/com/example/mycasinoapplication/view/BaccaratScreen.kt
+++ app/src/main/java/com/example/mycasinoapplication/view/BaccaratScreen.kt
@@ -1,8 +1,11 @@
 package com.example.mycasinoapplication.view
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,14 +16,22 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.FloatingActionButtonDefaults
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
+import com.example.mycasinoapplication.R
+import com.example.mycasinoapplication.model.DrawRecepient
 import com.example.mycasinoapplication.viewmodel.BaccaratViewModel
 
 @Composable
@@ -29,26 +40,8 @@ fun BaccaratScreen(viewModel: BaccaratViewModel) {
     val bankerHand by viewModel.bankerHand.collectAsState()
     val playerHand by viewModel.playerHand.collectAsState()
     Column(modifier = Modifier.fillMaxSize()) {
-        Row(modifier = Modifier.fillMaxWidth().height(200.dp).background(Color.Green).padding(8.dp)) {
-            LazyRow() {
-                Log.d("Meow", "banker: ${bankerHand.size} ${(bankerHand?.sumOf { it.value })?.mod(10)}")
-                items(bankerHand) {
-                    Log.d("Meow", "Banker ${it.getValueName()} ${it.suit}")
-                    val painter = rememberImagePainter(data = it.cardImage)
-                    Image(painter, "Banker ${it.getValueName()} ${it.suit}", modifier = Modifier.width(100.dp))
-                }
-            }
-        }
-        Row(modifier = Modifier.fillMaxWidth().height(200.dp).background(Color.Green).padding(8.dp)) {
-            LazyRow() {
-                Log.d("Meow", "banker: ${playerHand.size} ${(playerHand.sumOf { it.value }).mod(10)}")
-                items(playerHand) {
-                    Log.d("Meow", "Player ${it.getValueName()} ${it.suit}")
-                    val painter = rememberImagePainter(data = it.cardImage)
-                    Image(painter, "Player ${it.getValueName()} ${it.suit}", modifier = Modifier.width(100.dp))
-                }
-            }
-        }
+        Hand(viewModel, recipient = DrawRecepient.BANKER, height = 200)
+        Hand(viewModel, recipient = DrawRecepient.PLAYER, height = 200)
         Button(
             onClick = {
                 viewModel.clearHands()
@@ -60,5 +53,79 @@ fun BaccaratScreen(viewModel: BaccaratViewModel) {
         ) {
             Text(text = "DEAL")
         }
+    }
+}
+
+@Composable
+fun Hand(viewModel: BaccaratViewModel, recipient: DrawRecepient, height: Int) {
+    val hand by when (recipient) {
+        DrawRecepient.BANKER -> viewModel.bankerHand.collectAsState()
+        else -> viewModel.playerHand.collectAsState()
+    }
+    val playerTotal by viewModel.playerTotal.collectAsState()
+    val bankerTotal by viewModel.bankerTotal.collectAsState()
+    Column(modifier = Modifier.height(height = (height).dp).fillMaxWidth().background(Color.Green)) {
+        Text(
+            recipient.toString() + " " + when (recipient) {
+                DrawRecepient.BANKER -> bankerTotal.toString()
+                DrawRecepient.PLAYER -> playerTotal.toString()
+            },
+            modifier = Modifier.fillMaxWidth()
+                .background(Color.Transparent),
+            fontSize = 32.sp,
+            fontFamily = FontFamily.SansSerif,
+            color = Color.Red,
+        )
+        Box() {
+            LazyRow(modifier = Modifier.fillMaxWidth().padding(8.dp)) {
+                Log.d("Meow", "banker: ${hand.size} ${(hand.sumOf { it.value }).mod(10)}")
+                items(hand) {
+                    Log.d("Meow", "Banker ${it.getValueName()} ${it.suit}")
+                    val painter = rememberImagePainter(data = it.cardImage)
+                    Column() {
+                        AnimatedVisibility(
+                            visible = true,
+                            enter = fadeIn(initialAlpha = 0.2f),
+                        ) {
+                            Image(
+                                painter,
+                                "Banker ${it.getValueName()} ${it.suit}",
+                                modifier = Modifier.width(100.dp),
+                            )
+                        }
+                    }
+                }
+            }
+            Log.d("Meow", "$recipient card count ${hand.size}")
+            if (hand.size > 0) {
+                FloatingActionButton(
+                    onClick = {
+                        viewModel.drawCard(recipient)
+                    },
+                    Modifier.align(Alignment.BottomEnd).padding(16.dp),
+                    backgroundColor = Color.Blue,
+                    contentColor = Color.White,
+                    content = {
+                        val hitIcon =
+                            rememberImagePainter(data = R.drawable.baseline_plus_one_24)
+                        Icon(
+                            hitIcon,
+                            contentDescription = "Hit",
+                            modifier = Modifier.height(50.dp).padding(8.dp),
+                        )
+                    },
+                    elevation = FloatingActionButtonDefaults.elevation(
+                        defaultElevation = 8.dp,
+                        pressedElevation = 16.dp,
+                    ),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Bonus(viewModel: BaccaratViewModel) {
+    Row(modifier = Modifier.fillMaxWidth()) {
     }
 }
--- app/src/main/java/com/example/mycasinoapplication/viewmodel/BaccaratViewModel.kt
+++ app/src/main/java/com/example/mycasinoapplication/viewmodel/BaccaratViewModel.kt
@@ -11,6 +11,10 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class BaccaratViewModel : ViewModel() {
     private val deck = mutableListOf<Card>()
+    private var _playerTotal = MutableStateFlow<Int>(0)
+    var playerTotal = _playerTotal.asStateFlow()
+    private var _bankerTotal = MutableStateFlow<Int>(0)
+    var bankerTotal = _bankerTotal.asStateFlow()
     private val _bankerHand = MutableStateFlow<MutableList<Card>>(mutableListOf())
     val bankerHand = _bankerHand.asStateFlow()
     private val _playerHand = MutableStateFlow<MutableList<Card>>(mutableListOf())
@@ -21,19 +25,21 @@ class BaccaratViewModel : ViewModel() {
         val card = deck.get(index)
         when (recipient) {
             DrawRecepient.BANKER -> {
-                _bankerHand.value.add(card)
+                _bankerHand.value = _bankerHand.value.toMutableList().apply { add(card) }
+                _bankerTotal.value = _bankerHand.value.sumOf { it.value }.mod(10)
             }
             DrawRecepient.PLAYER -> {
-                _playerHand.value.add(card)
+                _playerHand.value = _playerHand.value.toMutableList().apply { add(card) }
+                _playerTotal.value = _playerHand.value.sumOf { it.value }.mod(10)
             }
         }
-        deck.remove(card)
+        deck.removeAt(index)
     }
 
     fun clearHands() {
         Log.d("Meow", "CLEAR!")
-        _bankerHand.value.removeAll { true }
-        _playerHand.value.removeAll { true }
+        _bankerHand.value = _bankerHand.value.toMutableList().apply { removeAll { true } }
+        _playerHand.value = _playerHand.value.toMutableList().apply { removeAll { true } }
         Log.d("Meow", "clear > Playerhand count ${playerHand.value.size}")
         Log.d("Meow", "clear > Bankerhand count ${bankerHand.value.size}")
     }
@@ -49,7 +55,20 @@ class BaccaratViewModel : ViewModel() {
     }
 
     init {
-        addDeck()
+        loadShoe(5)
+    }
+    fun loadShoe(deckCount: Int) {
+        deck.clear()
+        for (i in 1..deckCount) {
+            addDeck()
+        }
+    }
+
+    fun hit(hitter: DrawRecepient) {
+        when (hitter) {
+            DrawRecepient.BANKER -> {}
+            DrawRecepient.PLAYER -> {}
+        }
     }
 
     private fun addDeck() {
@@ -63,9 +82,9 @@ class BaccaratViewModel : ViewModel() {
         deck.add(Card(Suits.CLUB, R.drawable.clubs_8, 8))
         deck.add(Card(Suits.CLUB, R.drawable.clubs_9, 9))
         deck.add(Card(Suits.CLUB, R.drawable.clubs_10, 10))
-        deck.add(Card(Suits.CLUB, R.drawable.clubs_jack, 11))
-        deck.add(Card(Suits.CLUB, R.drawable.clubs_queen, 12))
-        deck.add(Card(Suits.CLUB, R.drawable.clubs_king, 13))
+        deck.add(Card(Suits.CLUB, R.drawable.c11b, 0))
+        deck.add(Card(Suits.CLUB, R.drawable.c12b, 0))
+        deck.add(Card(Suits.CLUB, R.drawable.c13b, 0))
 
         deck.add(Card(Suits.SPADE, R.drawable.spades_ace, 1))
         deck.add(Card(Suits.SPADE, R.drawable.spades_2, 2))
@@ -77,9 +96,9 @@ class BaccaratViewModel : ViewModel() {
         deck.add(Card(Suits.SPADE, R.drawable.spades_8, 8))
         deck.add(Card(Suits.SPADE, R.drawable.spades_9, 9))
         deck.add(Card(Suits.SPADE, R.drawable.spades_10, 10))
-        deck.add(Card(Suits.SPADE, R.drawable.spades_jack, 11))
-        deck.add(Card(Suits.SPADE, R.drawable.spades_queen, 12))
-        deck.add(Card(Suits.SPADE, R.drawable.spades_king, 13))
+        deck.add(Card(Suits.SPADE, R.drawable.s11b, 0))
+        deck.add(Card(Suits.SPADE, R.drawable.s12b, 0))
+        deck.add(Card(Suits.SPADE, R.drawable.s13b, 0))
 
         deck.add(Card(Suits.HEART, R.drawable.hearts_ace, 1))
         deck.add(Card(Suits.HEART, R.drawable.hearts_2, 2))
@@ -91,9 +110,9 @@ class BaccaratViewModel : ViewModel() {
         deck.add(Card(Suits.HEART, R.drawable.hearts_8, 8))
         deck.add(Card(Suits.HEART, R.drawable.hearts_9, 9))
         deck.add(Card(Suits.HEART, R.drawable.hearts_10, 10))
-        deck.add(Card(Suits.HEART, R.drawable.hearts_jack, 11))
-        deck.add(Card(Suits.HEART, R.drawable.hearts_queen, 12))
-        deck.add(Card(Suits.HEART, R.drawable.hearts_king, 13))
+        deck.add(Card(Suits.HEART, R.drawable.h11b, 0))
+        deck.add(Card(Suits.HEART, R.drawable.h12b, 0))
+        deck.add(Card(Suits.HEART, R.drawable.h13b, 0))
 
         deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_ace, 1))
         deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_2, 2))
@@ -105,8 +124,8 @@ class BaccaratViewModel : ViewModel() {
         deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_8, 8))
         deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_9, 9))
         deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_10, 10))
-        deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_jack, 11))
-        deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_queen, 12))
-        deck.add(Card(Suits.DIAMOND, R.drawable.diamonds_king, 13))
+        deck.add(Card(Suits.DIAMOND, R.drawable.d11b, 0))
+        deck.add(Card(Suits.DIAMOND, R.drawable.d12b, 0))
+        deck.add(Card(Suits.DIAMOND, R.drawable.d13b, 0))
     }
 }
